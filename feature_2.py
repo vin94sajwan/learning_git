a554ab07c92d4e2f2fd02df7f8cddd37270b91f8
Vineet Sajwan <31848562+vin94sajwan@users.noreply.github.com>
2023-03-20 23:46:52 +0530

    Feature 2 (#2)
    * Implemented feature_2?
    
    * changes in feature_2
    
    * Update feature_2.py
    
    * space change feature_2.py

--- feature_2.py
+++ feature_2.py
@@ -1,8 +1,12 @@
+
 rows = int(input("Enter number of rows: "))
 
 for i in range(rows):
     for j in range(i+1):
         print("* ", end="")
+
         print("Hello New World")
+        print("Hello World")
+        print("Hello World")
 
     print("\n")
