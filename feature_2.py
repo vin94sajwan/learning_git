2b80dbae1412a190a3c0204d28d0f91a641d1285
Vineet Sajwan <31848562+vin94sajwan@users.noreply.github.com>
2023-03-20 23:15:09 +0530

    Add files via upload

---
+++ feature_2.py
@@ -0,0 +1,9 @@
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+        print("Hello World")
+        print("Hello World")
+
+    print("\n")
