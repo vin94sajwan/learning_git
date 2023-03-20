c1a238d193836f55815f550f7b2be5c4389507c9
Vineet Sajwan <31848562+vin94sajwan@users.noreply.github.com>
2023-03-20 23:17:42 +0530

    Update feature_2.py

--- feature_2.py
+++ feature_2.py
@@ -3,7 +3,6 @@ rows = int(input("Enter number of rows: "))
 for i in range(rows):
     for j in range(i+1):
         print("* ", end="")
-        print("Hello World")
-        print("Hello World")
+        print("Hello New World")
 
     print("\n")
